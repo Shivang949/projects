01c1ea0f4347a646b4e462c4d484c30bebb82f24
Shivang Sharma <shivangsharma@Shivangs-MacBook-Air.local>
2026-04-30 00:06:40 +0530

    Added node server for get request

--- backend/test.js
+++ backend/test.js
@@ -2,9 +2,32 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.headers, req.method);
+
+    if (req.url === '/'){
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Response</title></head>');
+    res.write('<body><h1>Heyyy this is my first server code</h1></body>');
+    res.write('</html>');
+    res.end();
+    }else if (req.url === '/second') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My Second Response</title></head>');
+        res.write('<body><h1>Heyyy this is my Second server code</h1></body>');
+        res.write('</html>');
+        res.end();
+    } else if (req.url === '/third') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My Third Response</title></head>');
+        res.write('<body><h1>Heyyy this is my Third server code</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 
 const PORT = 3001;
 server.listen(PORT, () => {
-    console.log('Server has started on address http://localhost:${PORT}');
+    console.log('Server has started on address http://localhost:3001');
 });
